82c3faabb2ccf3b245aa765b43c21346439f9b19
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-05-07 20:11:52 +0100

    draw method added to Base class allowing it to render

--- flappyBird.py
+++ flappyBird.py
@@ -151,6 +151,10 @@ class Base:
         if self.x2 + self.WIDTH < 0:
             self.x2 = self.x1 + self.WIDTH
 
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
 
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
