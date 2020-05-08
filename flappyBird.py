69bfe196328d83aac70d9d873b88eb9fff231838
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-05-08 15:54:05 +0100

    altering functionality to add fitness to a bird that progresses through pipes or removes bird when collision occurs

--- flappyBird.py
+++ flappyBird.py
@@ -205,9 +205,12 @@ def main(genomes, config):
         add_pipe = False
         rem = []
         for pipe in pipes:
-            for bird in birds:
+            for x, bird in enumerate(birds):
                 if pipe.collide(bird):
-                    pass
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
 
                 if not pipe.passed and pipe.x < bird.x:
                     pipe.passed = True
@@ -220,14 +223,18 @@ def main(genomes, config):
 
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fintess += 5
             pipes.append(Pipe(700))
 
         for r in rem:
             pipes.remove(r)
 
-        for bird in birds:
+        for x, bird in enumerate(birds):
             if bird.y + bird.img.get_height() >= 730:
-                pass
+                birds.pop(x)
+                nets.pop(x)
+                ge.pops(x)
 
         base.move()
         draw_window(win, bird, pipes, base, score)
